eb206a4ba98d9d24c7bb40e3a8fc74fef9e0942d
Canto Ostinato <astrohavoc@gmail.com>
2017-10-04 02:08:35 +0800

    Initial commit

---
+++ src/NQueens.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module NQueens where
+
+import Data.SBV
+
+nqueens :: Int -> IO AllSatResult
+nqueens n =
+  allSat $ do
+    cols :: [SInteger] <- mkFreeVars n
+    pure $
+      bAll (`inRange` (0, fromIntegral $ n - 1)) cols &&&
+      bAll
+        (\op ->
+           distinct
+             [ row `op` col
+             | (row, col) <-
+                 [ (row, col)
+                 | row <- map fromIntegral [0 .. n - 1]
+                 | col <- cols
+                 ]
+             ])
+        [(+), (-), curry snd]
