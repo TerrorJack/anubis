714f2100dfdf38a191e1fc1e2f0c6ee078929a83
Canto Ostinato <astrohavoc@gmail.com>
2017-10-27 18:38:41 +0800

    Implement addLibBuildInfo & runLBIProgram

--- src/SetupUtils.hs
+++ src/SetupUtils.hs
@@ -2,11 +2,16 @@ module SetupUtils
   ( getUniqueComponentLocalBuildInfo
   , getLibComponentLocalBuildInfo
   , getLibInstallDirs
+  , addLibBuildInfo
+  , runLBIProgram
   ) where
 
 import Data.Map
 import Distribution.PackageDescription
 import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Program
+import Distribution.Simple.Setup
+import Distribution.Verbosity
 
 getUniqueComponentLocalBuildInfo ::
      LocalBuildInfo -> ComponentName -> ComponentLocalBuildInfo
@@ -29,3 +34,15 @@ getLibInstallDirs lbi =
     lbi
     (componentUnitId $ getLibComponentLocalBuildInfo lbi)
     NoCopyDest
+
+addLibBuildInfo :: LocalBuildInfo -> BuildInfo -> LocalBuildInfo
+addLibBuildInfo lbi bi =
+  lbi
+  {localPkgDescr = updatePackageDescription (Just bi, []) $ localPkgDescr lbi}
+
+runLBIProgram :: LocalBuildInfo -> Program -> [ProgArg] -> IO ()
+runLBIProgram lbi prog =
+  runDbProgram
+    (fromFlagOrDefault normal $ configVerbosity $ configFlags lbi)
+    prog
+    (withPrograms lbi)
